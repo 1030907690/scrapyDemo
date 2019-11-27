65c44d60dbbd6a9350adea02484c8798094e6325
zhouzhongqing <1030907690@qq.com>
2019-11-27 17:29:26 +0800

    1

--- parse/parse_file.py
+++ parse/parse_file.py
@@ -8,6 +8,7 @@ from parse.Video import *
 
 
 '''
+https://blog.csdn.net/qmhball/article/details/79364684
 添加水印
 ffmpeg -i index.mp4 -acodec copy -b:v 548k -vf "movie=logo.png[watermark];[in][watermark]overlay=20:20" output.mp4
 说明 
